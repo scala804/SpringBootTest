0c0ab6ba14d8aaf231bf570d43876e44cad2febe
scala804 <scala804@163.com>
2019-02-13 20:49:30 +0800

    提交springBoottest

---
+++ src/main/java/com/springBoot/scalaTest/TestApplication.java
@@ -0,0 +1,17 @@
+package com.springBoot.scalaTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 描述：
+ * Create By
+ */
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class);
+    }
+}
+
